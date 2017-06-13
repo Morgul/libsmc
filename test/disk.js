cbb873a5aa84b68b318a2f6c8ea891e900ad8a65
Brendan Ashworth <brendan.ashworth@me.com>
2017-06-12 19:40:46 -0700

    remove ES6 features
    They aren't compatible with node v0.11, v0.12. Things like const, let,
    and backticks.

--- test/disk.js
+++ test/disk.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const test = require('tape');
-const smc = require('../');
+var test = require('tape');
+var smc = require('../');
 
 test('isOpticalDiskDriveFull()', function(t) {
     t.plan(1);
 
     // isOpticalDiskDriveFull returns a boolean
-    let status = smc.isOpticalDiskDriveFull();
+    var status = smc.isOpticalDiskDriveFull();
     t.ok(status === true || status === false, 'must return boolean');
 });
 
@@ -15,7 +15,7 @@ test('isBatteryPowered()', function(t) {
     t.plan(1);
 
     // isBatteryPowered returns a boolean
-    let status = smc.isBatteryPowered();
+    var status = smc.isBatteryPowered();
     t.ok(status === true || status === false, 'must return boolean');
 });
 
@@ -23,7 +23,7 @@ test('getNumberOfFans()', function(t) {
     t.plan(3);
 
     // getNumberOfFans returns an integer
-    let num = smc.getNumberOfFans();
+    var num = smc.getNumberOfFans();
     t.ok(typeof num === 'number');
 
     if (num == 0)
@@ -44,12 +44,12 @@ test('getFan()', function(t) {
     t.plan(3);
 
     // If there are no fans, skip.
-    let num = smc.getNumberOfFans();
+    var num = smc.getNumberOfFans();
     if (num == 0)
         t.skip('no fans on machine');
 
     // Get the first fan and just test that.
-    let info = smc.getFan(0);
+    var info = smc.getFan(0);
 
     t.ok(typeof info === 'object');
     t.ok(typeof info.name === 'string');
